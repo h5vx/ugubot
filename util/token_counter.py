29416d0f4255782a0bbf9dab09b569cca1ac80c3
h5vx <h5v@protonmail.com>
2023-03-27 23:11:06 +0400

    Fix token counting

--- util/token_counter.py
+++ util/token_counter.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 
 def get_encoder_for_model(model):
     if model == "gpt-3.5-turbo":
-        return tiktoken.encoding_for_model("gpt-3.5-turbo-0301") + 8
+        return tiktoken.encoding_for_model("gpt-3.5-turbo-0301")
 
     if model == "gpt-4":
         return tiktoken.encoding_for_model("gpt-4-0314")
@@ -21,16 +21,12 @@ def get_encoder_for_model(model):
 
 def count_tokens_for_message(encoder, messages):
     """Returns the number of tokens used by a list of messages."""
-    # every message follows <|start|>{role/name}\n{content}<|end|>\n
-    tokens_per_message = 4
-    tokens_per_name = -1  # if there's a name, the role is omitted
+    tokens_per_message = 8
     num_tokens = 0
 
     for message in messages:
         num_tokens += tokens_per_message
-        for key, value in message.items():
+        for _, value in message.items():
             num_tokens += len(encoder.encode(value))
-            if key == "name":
-                num_tokens += tokens_per_name
-    num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
+
     return num_tokens
